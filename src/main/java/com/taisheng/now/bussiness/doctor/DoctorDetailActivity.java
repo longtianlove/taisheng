7d3a0bf6ed0ed466836af0b78ddbdc586694dcd9
龙 <bingodongtian@gmail.com>
2019-07-01 14:37:01 +0800

    temp

---
+++ src/main/java/com/taisheng/now/bussiness/doctor/DoctorDetailActivity.java
@@ -0,0 +1,16 @@
+package com.taisheng.now.bussiness.doctor;
+
+import android.os.Bundle;
+
+import com.taisheng.now.base.BaseActivity;
+
+/**
+ * Created by dragon on 2019/7/1.
+ */
+
+public class DoctorDetailActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
