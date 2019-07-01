7ea128af1b4f209114a8756af84824778e22b0b6
龙 <bingodongtian@gmail.com>
2019-07-01 17:14:32 +0800

    temp

--- src/main/java/com/taisheng/now/bussiness/doctor/DoctorDetailActivity.java
+++ src/main/java/com/taisheng/now/bussiness/doctor/DoctorDetailActivity.java
@@ -1,8 +1,11 @@
 package com.taisheng.now.bussiness.doctor;
 
 import android.os.Bundle;
+import android.view.Window;
 
+import com.taisheng.now.R;
 import com.taisheng.now.base.BaseActivity;
+import com.taisheng.now.view.chenjinshi.StatusBarUtil;
 
 /**
  * Created by dragon on 2019/7/1.
@@ -12,5 +15,27 @@ public class DoctorDetailActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+
+        //沉浸式代码配置
+        //当FitsSystemWindows设置 true 时，会在屏幕最上方预留出状态栏高度的 padding
+        StatusBarUtil.setRootViewFitsSystemWindows(this, true);
+        //设置状态栏透明
+        StatusBarUtil.setTranslucentStatus(this);
+        //一般的手机的状态栏文字和图标都是白色的, 可如果你的应用也是纯白色的, 或导致状态栏文字看不清
+        //所以如果你是这种情况,请使用以下代码, 设置状态使用深色文字图标风格, 否则你可以选择性注释掉这个if内容
+        if (!StatusBarUtil.setStatusBarDarkTheme(this, true)) {
+            //如果不支持设置深色风格 为了兼容总不能让状态栏白白的看不清, 于是设置一个状态栏颜色为半透明,
+            //这样半透明+白=灰, 状态栏的文字能看得清
+            StatusBarUtil.setStatusBarColor(this, 0x55000000);
+        }
+        //用来设置整体下移，状态栏沉浸
+        StatusBarUtil.setRootViewFitsSystemWindows(this, false);
+
+
+        setContentView(R.layout.activity_doctor_detail);
     }
 }
---
+++ src/main/java/com/taisheng/now/bussiness/me/FillInMessageActivity.java
@@ -0,0 +1,18 @@
+package com.taisheng.now.bussiness.me;
+
+import android.os.Bundle;
+
+import com.taisheng.now.R;
+import com.taisheng.now.base.BaseActivity;
+
+/**
+ * Created by dragon on 2019/7/1.
+ */
+
+public class FillInMessageActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fillinmessage);
+    }
+}
