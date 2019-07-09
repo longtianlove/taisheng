9647e3213e65219a9cfbacc928a1e298bd547b36
龙 <bingodongtian@gmail.com>
2019-07-09 09:52:47 +0800

    刷新图表 答题页面

--- src/main/java/com/taisheng/now/bussiness/healthfiles/HealthQuestionActivity.java
+++ src/main/java/com/taisheng/now/bussiness/healthfiles/HealthQuestionActivity.java
@@ -11,6 +11,7 @@ import com.taisheng.now.R;
 import com.taisheng.now.base.BaseActivity;
 import com.taisheng.now.base.BaseBean;
 import com.taisheng.now.bussiness.bean.post.QuestionPostBean;
+import com.taisheng.now.bussiness.bean.result.AssessmentOptionsList;
 import com.taisheng.now.bussiness.bean.result.QuestionResultBean;
 import com.taisheng.now.bussiness.bean.result.Records;
 import com.taisheng.now.bussiness.user.UserInstance;
@@ -34,10 +35,15 @@ public class HealthQuestionActivity extends BaseActivity {
     TextView tv_question;
 
     View ll_a;
+    TextView tv_a;
     View ll_b;
+    TextView tv_b;
     View ll_c;
+    TextView tv_c;
     View ll_d;
+    TextView tv_d;
     View ll_e;
+    TextView tv_e;
 
     QuestionResultBean allBean;
     List<Records> records;
@@ -64,21 +70,80 @@ public class HealthQuestionActivity extends BaseActivity {
         tv_all_size = (TextView) findViewById(R.id.tv_all_size);
         tv_question = (TextView) findViewById(R.id.tv_question);
         ll_a = findViewById(R.id.ll_a);
+        ll_a.setOnClickListener(listener);
+        tv_a = (TextView) findViewById(R.id.tv_a);
         ll_b = findViewById(R.id.ll_b);
+        ll_b.setOnClickListener(listener);
+        tv_b = (TextView) findViewById(R.id.tv_b);
         ll_c = findViewById(R.id.ll_c);
+        ll_c.setOnClickListener(listener);
+        tv_c = (TextView) findViewById(R.id.tv_c);
         ll_d = findViewById(R.id.ll_d);
+        ll_d.setOnClickListener(listener);
+        tv_d = (TextView) findViewById(R.id.tv_d);
         ll_e = findViewById(R.id.ll_e);
-
-
+        ll_e.setOnClickListener(listener);
+        tv_e = (TextView) findViewById(R.id.tv_e);
     }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(question==null){
+                return;
+            }
+            List<AssessmentOptionsList> assessmentOptionsList=question.assessmentOptionsList;
+            if(assessmentOptionsList==null||assessmentOptionsList.size()<=0){
+                return;
+            }
+            String result="";
+            switch (v.getId()) {
+                case R.id.ll_a:
+                    tv_a.setEnabled(true);
+                    result=assessmentOptionsList.get(0).id;
+                    break;
+                case R.id.ll_b:
+                    tv_b.setEnabled(true);
+                    result=assessmentOptionsList.get(1).id;
+                    break;
+                case R.id.ll_c:
+                    tv_c.setEnabled(true);
+                    result=assessmentOptionsList.get(2).id;
+
+                    break;
+                case R.id.ll_d:
+                    tv_d.setEnabled(true);
+                    result=assessmentOptionsList.get(3).id;
+
+                    break;
+                case R.id.ll_e:
+                    tv_e.setEnabled(true);
+                    result=assessmentOptionsList.get(4).id;
+                    break;
+            }
+            answersResult=answersResult+result+",";
+
+            position++;
+            if (position < records.size()) {
+                updateView();
+            } else {
+                //todo 提交答案跳转页面
+                answersResult=answersResult.substring(0,answersResult.length()-1);
+                answersResult=answersResult+"]";
+            }
+        }
+    };
 
     String assessmentType;
     String subjectdbType;
+    String answersResult = "[";//答案结果
 
     void initData() {
         Intent intent = getIntent();
         assessmentType = intent.getStringExtra("assessmentType");
         subjectdbType = "1";
+        answersResult = "[";
+        position = 0;
         QuestionPostBean bean = new QuestionPostBean();
         bean.userId = UserInstance.getInstance().getUid();
         bean.token = UserInstance.getInstance().getToken();
@@ -125,14 +190,63 @@ public class HealthQuestionActivity extends BaseActivity {
     }
 
     void updateAnswer() {
+        tv_a.setEnabled(false);
+        tv_b.setEnabled(false);
+        tv_c.setEnabled(false);
+        tv_d.setEnabled(false);
+        tv_e.setEnabled(false);
         switch (question.assessmentOptionsList.size()) {
             case 1:
                 ll_a.setVisibility(View.VISIBLE);
-
+                tv_a.setText(question.assessmentOptionsList.get(0).optionsValue);
                 ll_b.setVisibility(View.GONE);
                 ll_c.setVisibility(View.GONE);
                 ll_d.setVisibility(View.GONE);
                 ll_e.setVisibility(View.GONE);
+                break;
+            case 2:
+                ll_a.setVisibility(View.VISIBLE);
+                tv_a.setText(question.assessmentOptionsList.get(0).optionsValue);
+                ll_b.setVisibility(View.VISIBLE);
+                tv_b.setText(question.assessmentOptionsList.get(1).optionsValue);
+                ll_c.setVisibility(View.GONE);
+                ll_d.setVisibility(View.GONE);
+                ll_e.setVisibility(View.GONE);
+                break;
+            case 3:
+                ll_a.setVisibility(View.VISIBLE);
+                tv_a.setText(question.assessmentOptionsList.get(0).optionsValue);
+                ll_b.setVisibility(View.VISIBLE);
+                tv_b.setText(question.assessmentOptionsList.get(1).optionsValue);
+                ll_c.setVisibility(View.VISIBLE);
+                tv_c.setText(question.assessmentOptionsList.get(2).optionsValue);
+
+                ll_d.setVisibility(View.GONE);
+                ll_e.setVisibility(View.GONE);
+                break;
+            case 4:
+                ll_a.setVisibility(View.VISIBLE);
+                tv_a.setText(question.assessmentOptionsList.get(0).optionsValue);
+                ll_b.setVisibility(View.VISIBLE);
+                tv_b.setText(question.assessmentOptionsList.get(1).optionsValue);
+                ll_c.setVisibility(View.VISIBLE);
+                tv_c.setText(question.assessmentOptionsList.get(2).optionsValue);
+                ll_d.setVisibility(View.VISIBLE);
+                tv_d.setText(question.assessmentOptionsList.get(3).optionsValue);
+                ll_e.setVisibility(View.GONE);
+                break;
+            case 5:
+                ll_a.setVisibility(View.VISIBLE);
+                tv_a.setText(question.assessmentOptionsList.get(0).optionsValue);
+                ll_b.setVisibility(View.VISIBLE);
+                tv_b.setText(question.assessmentOptionsList.get(1).optionsValue);
+                ll_c.setVisibility(View.VISIBLE);
+                tv_c.setText(question.assessmentOptionsList.get(2).optionsValue);
+                ll_d.setVisibility(View.VISIBLE);
+                tv_d.setText(question.assessmentOptionsList.get(3).optionsValue);
+                ll_e.setVisibility(View.VISIBLE);
+                tv_e.setText(question.assessmentOptionsList.get(4).optionsValue);
+
                 break;
         }
     }
