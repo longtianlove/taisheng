05f215ed467fec896527864f5ea4e489f3103d50
龙 <bingodongtian@gmail.com>
2019-07-17 15:47:40 +0800

    隐私协议

--- src/main/java/com/taisheng/now/bussiness/me/AboutUsActivity.java
+++ src/main/java/com/taisheng/now/bussiness/me/AboutUsActivity.java
@@ -1,11 +1,13 @@
 package com.taisheng.now.bussiness.me;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.taisheng.now.R;
 import com.taisheng.now.base.BaseActivity;
+import com.taisheng.now.bussiness.user.LoginActivity;
 import com.taisheng.now.util.Apputil;
 
 /**
@@ -15,6 +17,8 @@ import com.taisheng.now.util.Apputil;
 public class AboutUsActivity extends BaseActivity {
     View iv_back;
     TextView tv_version;
+    View ll_fuwuxieyi;
+    View ll_yisixieyi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +39,24 @@ public class AboutUsActivity extends BaseActivity {
         tv_version = (TextView) findViewById(R.id.tv_version);
         String version = Apputil.getVersionName(this);
         tv_version.setText("版本号："+version);
+
+
+        ll_fuwuxieyi = findViewById(R.id.ll_fuwuxieyi);
+        ll_fuwuxieyi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutUsActivity.this, FuwuxieyiActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ll_yisixieyi=findViewById(R.id.ll_yisixieyi);
+        ll_yisixieyi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutUsActivity.this, YisixieyiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
