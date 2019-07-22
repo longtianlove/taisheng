ca839cda912800ac2775bf2c74c3e39469b48f82
龙 <bingodongtian@gmail.com>
2019-07-22 15:39:47 +0800

    手机号校验

--- src/main/java/com/taisheng/now/bussiness/user/LoginActivity.java
+++ src/main/java/com/taisheng/now/bussiness/user/LoginActivity.java
@@ -29,10 +29,14 @@ import com.taisheng.now.bussiness.me.FillInMessageSecondActivity;
 import com.taisheng.now.bussiness.me.FuwuxieyiActivity;
 import com.taisheng.now.util.DialogUtil;
 import com.taisheng.now.util.SPUtil;
+import com.taisheng.now.util.ToastUtil;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by dragon on 2019/6/27.
@@ -147,7 +151,7 @@ public class LoginActivity extends BaseFragmentActivity implements LoginView {
                     iv_shoujihao_guanbi.setVisibility(View.GONE);
                     btn_yanzhengma_login.setEnabled(false);
                 }
-                if (s.length() == 11 && messageWaitTime <= 1) {
+                if (isPhone(s.toString()) && messageWaitTime <= 1) {
                     btn_yanzhengma.setEnabled(true);
                 } else {
                     btn_yanzhengma.setEnabled(false);
@@ -353,6 +357,25 @@ public class LoginActivity extends BaseFragmentActivity implements LoginView {
             return false;
         }
         return true;
+    }
+
+
+    public static boolean isPhone(String phone) {
+        String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
+        if (phone.length() != 11) {
+//            MToast.showToast("手机号应为11位数");
+            return false;
+        } else {
+            Pattern p = Pattern.compile(regex);
+
+            Matcher m = p.matcher(phone);
+            boolean isMatch = m.matches();
+//            LogUtil.e(isMatch);
+            if (!isMatch) {
+                ToastUtil.showTost("请填入正确的手机号");
+            }
+            return isMatch;
+        }
     }
 
     @Override
