5aee6a3e64b9c8e3df0d4a0d3f450096783ebfac
龙 <bingodongtian@gmail.com>
2019-09-23 09:14:54 +0800

    temp

--- src/main/java/com/taisheng/now/Constants.java
+++ src/main/java/com/taisheng/now/Constants.java
@@ -47,20 +47,22 @@ public class Constants {
 
 
 //        public static String Host = "http://192.168.1.12:8080/";
-//                public static String Host = "http://192.168.1.17:8080/";
+
 //public static String Host = "http://192.168.1.18:8080/";
-        public static String Host = "http://152.136.26.41:8080/";
+//        public static String Host = "http://152.136.26.41:8080/";
 
 
         //        public static String File_Host = "http://120.24.152.121:9700/";
-        public static String File_Host = "http://152.136.26.41:8888/";
-//        public static String File_Host = "http://192.168.1.17:8080/jeecg-boot/";
-
 
 //        public static String WEB_SOCKET_URL="ws://192.168.1.18:8879";
-//        public static String WEB_SOCKET_URL="ws://192.168.1.17:8879";
+
+                public static String Host = "http://152.136.26.41:8080/";
+        public static String File_Host = "http://152.136.26.41:8888/";
         public static String WEB_SOCKET_URL = "ws://152.136.26.41:8879";
 
+//        public static String Host = "http://192.168.1.17:8080/";
+//        public static String File_Host = "http://192.168.1.17:8080/jeecg-boot/";
+//        public static String WEB_SOCKET_URL = "ws://192.168.1.17:8879";
 
 
         //用户相关
@@ -102,7 +104,6 @@ public class Constants {
 
             //邀请历史
             public static final String myrecommendshare = "jeecg-boot/app/extension/getExtensionManagement";
-
 
 
             //签到
